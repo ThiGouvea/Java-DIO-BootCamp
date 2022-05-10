bb5226f25168a6ba3a14f301664e57488369c126
ThiGouvea <thiagouvea90@gmail.com>
2022-05-10 09:27:20 -0300

    continuação das aulas em tipos de dados

---
+++ untitled/src/one/digitalinovation/basecamp/ExercicioTiposdeDados.java
@@ -0,0 +1,34 @@
+package one.digitalinovation.basecamp;
+
+public class ExercicioTiposdeDados {
+    public static void main(String[] args) {
+        byte bit1 = 10;
+        byte bit2 = 34;
+
+        short curto1 = 2000;
+        short curto2 = 4000;
+
+        int inteiro1 = 4035;
+        int inteiro2 = 285999;
+
+        long numeroLongo1 = 954578915781777152L;
+        long numeroLongo2 = 211324789123478973L;
+
+        float flutuante1 = 5.6F;
+        float flutuante2 = 6.6F;
+
+        double naoInteiro1 = 87.9;
+        double naoInteiro2 = 78.9;
+
+        char caractere1 = 'c';
+        char caractere2 = 'u';
+
+        String palavras1 = "Coisas";
+        String palavras2 = "Estranhas";
+
+        boolean verdadeiroOuFalso  = true;
+        boolean verdadeiroOuFalso2 = false;
+
+
+    }
+}
